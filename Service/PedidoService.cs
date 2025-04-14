3cbb04bdf52dc8a7262d3aa6c975fc45bf1a9318
Moquiutti <lucas.moquiutti@gmail.com>
2025-04-14 17:08:07 -0300

    Metodos adicionados dentro de PedidoService.cs.

--- Service/PedidoService.cs
+++ Service/PedidoService.cs
@@ -10,12 +10,14 @@ namespace Solutionkitchen.Service
         public List<Pedido> ObterTodos() => pedidos;
 
         public void CriarPedido(Pedido pedido) => pedidos.Add(pedido);
+        public void EditarPedido(Pedido pedido) => pedidos.Remove(pedido);
+        public void ValidarPedido(Pedido pedido) => pedidos.Find(p => p.Id == pedido.Id);
 
-        public void AtualizarStatus(int id, string novoStatus)
+    public void AtualizarStatus(int id, string novoStatus)
         {
             var pedido = pedidos.FirstOrDefault(p => p.Id == id);
             if (pedido != null) pedido.Status = novoStatus;
         }
     }
-
+    
 }
