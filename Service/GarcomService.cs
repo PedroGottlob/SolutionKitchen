373f34d231d44cf530eabe26d37ad98aa886e11a
Pedro <pedrogottlob@gmail.com>
2025-04-18 13:33:10 -0300

    Created Method ADD,EDIT, DELETE to order

--- Service/GarcomService.cs
+++ Service/GarcomService.cs
@@ -1,8 +1,31 @@
+using Solutionkitchen.Model;
+
 namespace Solutionkitchen.Service;
 
 public class GarcomService
 {
-   
+    private readonly PedidoService _pedidoService;
+
+    public GarcomService(PedidoService pedidoService)
+    {
+        _pedidoService = pedidoService;
+    }
+
+    public void CriarPedido(Pedido pedido)
+    {
+        _pedidoService.CriarPedido(pedido);
+    }
+
+    public void EditarPedido(int id,string novoPrato,int novaQuantidade, double novoPrecoUnitario, MetodoPagamento novoMetodoPagamento)
+    {
+        _pedidoService.EditarPedido(id,novoPrato,novaQuantidade,novoPrecoUnitario,novoMetodoPagamento);
+    }
+
+    public void RemoverPedido(int id)
+    {
+        _pedidoService.RemoverPedido(id);
+    }
+    
     public void PegarPrato(object sender, EventArgs e)
     {
         Console.WriteLine("Garçom: Pedido pronto, indo buscar!");
--- Service/PedidoService.cs
+++ Service/PedidoService.cs
@@ -10,10 +10,38 @@ namespace Solutionkitchen.Service
         public List<Pedido> ObterTodos() => pedidos;
 
         public void CriarPedido(Pedido pedido) => pedidos.Add(pedido);
-        public void EditarPedido(Pedido pedido) => pedidos.Remove(pedido);
-        public void ValidarPedido(Pedido pedido) => pedidos.Find(p => p.Id == pedido.Id);
 
-    public void AtualizarStatus(int id, string novoStatus)
+        public void EditarPedido(int id, string novoPrato, int novaQuantidade, double novoPrecoUnitario,
+            MetodoPagamento novoMetodoPagamento)
+        {
+            var pedido = pedidos.FirstOrDefault(p => p.Id == id);
+
+            if (pedido != null)
+            {
+                pedido.Prato = novoPrato;
+                pedido.Quantidade = novaQuantidade;
+                pedido.PrecoUnitario = novoPrecoUnitario;
+                pedido.MetodoPagamento = novoMetodoPagamento;
+
+                Console.WriteLine($"Pedido {id} atualizado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Pedido não encontrado.");
+            }
+        }
+
+        public void RemoverPedido(int id)
+        {
+            var pedido = pedidos.FirstOrDefault(p => p.Id == id);
+            if (pedido != null)
+            {
+                pedidos.Remove(pedido);
+                Console.WriteLine($"Pedido {id} removido com sucesso.");
+            }
+        }
+
+        public void AtualizarStatus(int id, string novoStatus)
         {
             var pedido = pedidos.FirstOrDefault(p => p.Id == id);
             if (pedido != null) pedido.Status = novoStatus;
